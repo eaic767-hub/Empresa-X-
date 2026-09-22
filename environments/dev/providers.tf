ee777def9ccc66be9ba8d1cd26b735d1084bf9da
einfa <eaic767@gmail.com>
2026-09-22 14:52:50 -0400

    feat: configuracion inicial empresa x

--- environments/dev/providers.tf
+++ environments/dev/providers.tf
@@ -4,17 +4,16 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~>5.0"
+      version = "~> 5.0"
     }
   }
 
-  #CONFIGURACIÓN DE BACKEND REMOTO 
-  #se identa para subir el tf primero y luego se quita el # para migrar el terraform.tfstate al bucket s3 con el lock state de DynamoDB_table
+  # CONFIGURACIÓN DE BACKEND REMOTO 
   backend "s3" {
     bucket       = "terraform-bucket-empresax-2026-v1"
     key          = "dev/terraform.tfstate"
     region       = "us-east-1"
-    use_lockfile = true #dynamoDB_table = "nombre" 
+    use_lockfile = true
     encrypt      = true
   }
 }
